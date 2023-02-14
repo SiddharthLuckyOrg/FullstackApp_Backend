8eacf06a3634b088d3a7e0bdfbf600a4d01786d6
Siddharth Lucky <siddharth.luckyreddy@gmail.com>
2023-02-13 21:28:43 -0500

    - User controller to use Get and post calls to the db

---
+++ src/main/java/com/lucky/fullstack_backend/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.lucky.fullstack_backend.controller;
+
+import com.lucky.fullstack_backend.model.User;
+import com.lucky.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/allUsers")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
